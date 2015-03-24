0d3397d86fece209f22563178ae5775f49f65569
matty-l <mjlevine@colby.edu>
2015-03-23 22:52:22 -0500

    Removed some commented-out code, simplified a wrapper function, elaborated on docstrings in nnff

--- nnff.py
+++ nnff.py
@@ -8,35 +8,28 @@
 from numpy import shape, array, ones, mean, sum, log, exp, max
 from numpy.random import random as rand
 
-from tanh_opt import tanh_opt
-from sigm import sigm
-from softmax import softmax
-
-from utils import add, size
+from utils import add
 
 def nnff(nn,x,y):
-	""" Performs a feedforward pass
+	""" Performs a feedforward pass.
+	
+	If this function throws an exception, it's almost definately because of
+	dimension mismatch between the weight matrices and x. Check to make sure
+	that the architecture you described in nnsetup actually fits with the 
+	size of the input set.
 	
 		Feeds forward the neural network to update layer activations, error, and loss
 	"""
 	
 	n = nn.n
-	m = size(x)[0]
+	m = x.shape[0]
 
 	x = add(ones((m,1)),x)
 	nn.a[0] = x
 	
 	# feed forward pass
 	for i in range(1,n-1):
-		if nn.activation_function == 'sigm':
-			# calculate the unit's outputs (including the bias term)
-			nn.a[i] = sigm(nn.a[i-1].dot(nn.W[i-1].transpose()))
-		elif nn.activation_function == 'tanh_opt':
-			try:
-				nn.a[i] = tanh_opt(nn.a[i-1].dot(nn.W[i-1].transpose()))
-			except Exception as e:
-				raise e # this seems silly, but sometimes it's useful to error-check here
-			
+		nn.a[i] = nn.activation_function_impl.feedforward(nn.a[i-1],nn.W[i-1])			
 			
 		# dropout
 		if nn.dropoutFraction > 0:
@@ -44,26 +37,18 @@ def nnff(nn,x,y):
 				nn.a[i] = nn.a[i] * (1-nn.dropoutFraction)
 				
 			else:
-				nn.dropOutMask[i] = (rand(size(nn.a[i]))>nn.dropoutFraction)
+				nn.dropOutMask[i] = (rand(nn.a[i].shape)>nn.dropoutFraction)
 				nn.a[i] = nn.a[i] * nn.dropOutMask[i]
 								
 		# add the bias term
 		nn.a[i] = add( ones((m,1)), nn.a[i] )
 		
 	nn.a[n-1] = nn.output_function.feedforward(nn.a[n-2],nn.W[n-2])
-	# if nn.output == 'sigm':		
-		# nn.a[n-1] = sigm(nn.a[n-2].dot(nn.W[n-2].transpose()))
-	# elif nn.output == 'softmax':
-		# nn.a[n-1] = softmax(nn.a[n-2].dot(nn.W[n-2].transpose()))
 
 	# error and loss
 	nn.e = y - nn.a[n-1]
 	
 	nn.L = nn.output_function.evaluate_loss( nn.e, m, nn.a[n-1], y )
-	# if nn.output == 'sigm':
-		# nn.L = 1/2 * sum(sum(nn.e * nn.e)) / m
-	# elif nn.output == 'softmax':
-		# nn.L = -sum(sum(y * log(nn.a[n-1]))) / m
 	
 	
 	
@@ -86,12 +71,12 @@ if __name__ == '__main__':
 	
 	if level == 'first':
 		first_activations = nn.a[0][0,1:]
-		print(size(first_activations))
+		print(first_activations.shape)
 		# print(first_activations)
 		toimage(first_activations.reshape((28,28))).show()
 	elif level == 'second':
 		second_activations = nn.a[1][0,1:]
-		print(size(second_activations))
+		print(second_activations.shape)
 		# print(second_activations)
 		toimage(second_activations.reshape((10,10))*255).show()
 	elif level == 'third':
